36ab20d0858705e75fc7dadd517d8ccc9b22c069
Hye Kyeong Jeong <54679626+hyekyeong123@users.noreply.github.com>
2020-01-29 00:17:29 +0900

    비밀정보 보호하기

--- README.md
+++ README.md
@@ -7,9 +7,43 @@
 2.  [npm init]
 3.  [npm install express --save]
 4.  몽고 DB 연결
+
     > mongoose 설치하기 **[npm install mongoose --save]**
     > model - 스키마를 감싸는 역할
+
+    ***
+
 5.  **[git init]**
-    > **[git status]**  
+
+    > **[git status]** 깃 상태 확인
     > Working Directory
-    > **[git add .]**
+    > .gitignore로 반드시 node_modules 빼줘야함
+    > **[git rm --cached node_modules -r]** > **[git add .]**
+    > Stagin Area
+    > **[git commit -m "User 모델 작성"]**
+    > Git repository(LOCAL)
+    > ssh 연결하기
+    > **[git remote add origin https://github.com/hyekyeong123/boiler.git]** > **[git push -u origin master]**
+
+---
+
+6. body-parser 설치하기
+
+    > **[npm install body-parser --save]**
+
+7. postman 만들기
+
+8. Register Route 만들기
+
+9. nodemon 설치하기
+   **[npm install nodemon --save-dev]**
+   이후 패키지 제이슨에다 설정
+
+10. 비밀정보 보호하기
+
+
+    <!--
+        declaration - 선언문, 맹세, 신고서
+        severity - 격렬, 혹독
+        statement - 성명, 진술, 입출금 내역서
+     -->
--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const app = express();
 const port = 3333;
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const config = require('./config/key')
+const { User } = require('./models/User');
+//------------------------------------------------------------------------------- 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://jhg097:2368wjdzxc@@hyekyeong-dnvtr.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('몽고디비가 성공적으로 연결되었습니다.'))
     .catch(err => console.log(err));
 //--------------------------- 
 app.get('/', (req, res) => res.send('안녕하세요.')); //루트 디렉토리
+
+
+//회원가입할때 필요한 정보들을 클라이언트에서 가져오면 그것들을 데이터 베이스에 넣어야함
+app.post('/register', (req, res) => {
+    const user = new User(req.body);
+    //회원가입할때 사용자 입력 정보
+    
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({success:true})
+    })
+    //몽고디비 내용 저장하기
+    
+});
+
 app.listen(port, () => console.log(`${port}포트에 잘 들어오셨습니다.`));
